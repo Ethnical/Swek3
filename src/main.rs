5fbb07745dec2b3386f1381207b30c2ec958872b
ethnical <ethnical@1337.fr>
2022-09-13 22:34:36 +0200

    clear warning + add modifuers crisk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 mod contract2interface;
-mod ethCallJson;
+mod eth_call_json;
 mod mempool_watcher;
+mod modifiers;
 #[derive(clap::Parser)]
 
 struct Cli {
@@ -28,6 +29,24 @@ struct MemPoolArgs {
     /// Path of the Json (e.g : --path /src/json.txt).
     interval: u128,
 }
+#[derive(clap::Parser)]
+struct LinkContractArgs {
+    #[clap(short, long)]
+    /// Link to the contract you want to analyze (e.g : --link https://etherscan.io/address/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984).
+    link: String,
+}
+#[derive(clap::Parser, Default)]
+struct ModifiersContractArgs {
+    #[clap(short, long)]
+    /// Path of the Solidity Contract (e.g : --path /src/example.sol).
+    path: String,
+    #[clap(short, long, default_value = "")]
+    /// select a modifier.
+    modifiers: String,
+    #[clap(short, long, default_value = "true")]
+    /// To not generate crisk markdown (e.g : --crisk false) by default is true.
+    crisk: String,
+}
 
 #[derive(clap::Subcommand)]
 enum Commands {
@@ -38,7 +57,11 @@ enum Commands {
     /// Convert Json output from EthCall_debug to Asm.
     Json2asm(EthCallJsonAsmArgs),
     /// Mempool Watcher with a custom intervals in nanoseconds.
-    Mempool_watcher(MemPoolArgs),
+    MempoolWatcher(MemPoolArgs),
+    /// Not implemented yet!
+    AnalyzeVerifiedContract(LinkContractArgs),
+    /// Tools to displays modifiers, crisk etc..
+    Modifiers(ModifiersContractArgs),
 }
 
 fn main() {
@@ -51,10 +74,17 @@ fn main() {
             println!("WeiConverter");
         }
         Commands::Json2asm(args) => {
-            ethCallJson::exec_module_json_to_asm(&args.path);
+            eth_call_json::exec_module_json_to_asm(&args.path);
         }
-        Commands::Mempool_watcher(args) => {
+        Commands::MempoolWatcher(args) => {
             mempool_watcher::exec_module_watcher_mempool(args.interval);
         }
+        Commands::AnalyzeVerifiedContract(_args) => {
+            println!("not implemented yet!");
+        }
+        Commands::Modifiers(args) => {
+            modifiers::exec_module_crisk(&args.path, &args.modifiers, &args.crisk);
+        } //  mempool_watcher::exec_module_watcher_mempool(args.interval);
+          // }
     }
 }
---
+++ src/modifiers.rs
@@ -0,0 +1,193 @@
+use ethers::prelude::artifacts::Node;
+use ethers::prelude::artifacts::NodeType;
+use ethers::{
+    prelude::error::SolcError,
+    solc::{CompilerOutput, Solc},
+};
+use semver::Version;
+use serde_json::{json, Value};
+#[allow(non_snake_case)]
+use std::collections::HashMap;
+
+use std::fs;
+
+pub fn exec_module_crisk(path: &str, modifiers_args: &str, crisk_bool: &str) {
+    // Let input be a valid "Standard Solidity Input JSON"
+    let contents = read_to_string(path);
+
+    //println!("File : {}", contents);
+    let version = parse_pragma_version(&contents);
+    println!("[+] Detected version is  {}", version);
+
+    //let custom_error = format!("Solc version {} not found", version);
+    let res = find_or_install_svm_version(version)
+        .expect("Error happend during the downloading of the version...");
+    //res.args = vec![String::from("ast-json")];
+
+    let json_comp_output = compile_to_ast(res, path);
+    let map_modifiers = print_modifiers(json_comp_output);
+    display_modifiers(
+        map_modifiers,
+        crisk_bool.to_string(),
+        modifiers_args.to_string(),
+    );
+}
+
+/*Return the interface for a sol contract given. */
+pub fn compile_to_ast(solc_perso: Solc, filename: &str) -> CompilerOutput {
+    let src = read_to_string(filename);
+    let solc_config = format!(
+        r#"{{
+            "language": "Solidity",
+            "sources": {{ "input.sol": {{ "content": {} }} }},
+            "settings": {{
+                "optimizer": {{ "enabled": {} }},
+                "libraries": {{ 
+                    "input.sol" : {{ {} }} 
+                }},
+                "outputSelection": {{
+                    "*": {{
+                        "*": [
+                            "evm.bytecode.object", "abi"
+                        ],
+                    "": [ "*" ] }}
+                }}
+            }}
+        }}"#,
+        json!(src),
+        false,
+        ""
+    );
+
+    let datas: Value = serde_json::from_str(&solc_config).unwrap(); //convert in type Value using the serd_json lib
+    let s = solc_perso; //Solc::default();
+    let json_compiled = s
+        .compile(&datas)
+        .expect("[-] Failed to compile the contract..");
+    return json_compiled;
+}
+
+pub fn print_modifiers(json_compiled: CompilerOutput) -> HashMap<String, Vec<String>> {
+    let mut modifiers_hashmap: HashMap<String, Vec<String>> = HashMap::new();
+
+    for elem in json_compiled.sources["input.sol"]
+        .clone()
+        .ast
+        .unwrap()
+        .nodes[contractname_to_id()] //weeird error with non usize why? I32 was not working?
+    .clone()
+    .nodes
+    {
+        if elem.node_type == NodeType::FunctionDefinition {
+            let name = elem.other["name"].clone();
+            let array_modifiers = get_modifiers_from_func(elem);
+            modifiers_hashmap.insert(name.to_string().replace("\"", ""), array_modifiers);
+        }
+    }
+    return modifiers_hashmap;
+}
+
+// else if (modifiers_args.is_empty()) {
+//     //No modifiers in params
+//     modifier_list.push(
+//         &modifier["modifierName"]["name"]
+//             .to_string()
+//             .replace("\"", ""),
+//     );
+// }
+
+//println!("{:#?}", modifier["modifierName"]["name"]);
+
+/*
+for modifier in  {
+    println!(modifier["name"]);
+}
+counter = counter + 1;*/
+pub fn display_modifiers(tab: HashMap<String, Vec<String>>, crisk: String, modifiers_args: String) {
+    let mut counter_mod = 0;
+    for elem in tab {
+        if !modifiers_args.is_empty() {
+            if elem.1.contains(&modifiers_args) {
+                if crisk == "true" {
+                    println!("- `{}` : ", elem.0);
+                }
+                if crisk == "false" {
+                    println!("{} | {:?}", elem.0, elem.1);
+                }
+
+                counter_mod += 1;
+            }
+        } else {
+            if crisk == "true" {
+                println!("- `{}` : ", elem.0);
+            }
+            if crisk == "false" {
+                println!("{} | {:?}", elem.0, elem.1);
+            }
+            counter_mod += 1;
+        }
+    }
+    if !modifiers_args.is_empty() {
+        if counter_mod != 0 {
+            println!(
+                "Number of match for the modifier \"{}\" : {}",
+                modifiers_args, counter_mod
+            );
+        } else {
+            println!("The number of match is equal to 0, for the modifier {}.\nThis probably an error of typo recheck correctly the modifier syntax! ", modifiers_args);
+        }
+    } else {
+        println!("Number of modifiers inside the contract : {}", counter_mod);
+    }
+}
+pub fn contractname_to_id() -> usize {
+    //TODO need to implement here.
+    return 12;
+}
+
+pub fn get_modifiers_from_func(elem: Node) -> Vec<String> {
+    // return the modifiers for a function given (Node).
+    //let mut counter_modifiers = 0;
+    let mut modifier_list = vec![];
+    for tab in elem.other["modifiers"].as_array() {
+        for modifier in tab {
+            //
+            //modifiers slected in arhgs through cli
+            modifier_list.push(
+                modifier["modifierName"]["name"]
+                    .clone()
+                    .to_string()
+                    .replace("\"", ""),
+            );
+        }
+    }
+    return modifier_list;
+}
+pub fn parse_pragma_version(content: &str) -> String {
+    let slices: Vec<&str> = content.split("pragma solidity ").collect();
+    let slices: Vec<&str> = slices.get(1).unwrap().split(";").collect();
+    let res = slices.get(0).unwrap().to_string().replace("^", "");
+    res
+}
+pub fn read_to_string(filename: &str) -> String {
+    //Add result String, None
+
+    fs::read_to_string(filename).expect(&format!(
+        "Something went wrong during the reading of : {}",
+        filename
+    ))
+}
+//eeeee
+pub fn find_or_install_svm_version(version: impl AsRef<str>) -> Result<Solc, SolcError> {
+    let version = version.as_ref();
+    if let Some(solc) = Solc::find_svm_installed_version(version)? {
+        println!("[+] Solc v{} already installed.", version);
+        Ok(solc)
+    } else {
+        println!(
+            "[+] Solc v{} not installed. Downloading & Installing the version now, Please wait...",
+            version
+        );
+        Ok(Solc::blocking_install(&version.parse::<Version>()?)?)
+    }
+}
